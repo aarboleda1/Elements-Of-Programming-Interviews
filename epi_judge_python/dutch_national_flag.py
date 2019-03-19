b2a50ff8caf5ceff81b136d6294788e976edad2a
Anton Arboleda <aarboleda@fb.com>
2019-03-19 09:09:54 -0700

    dutch_national_flag and is_valid_parens

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -6,10 +6,41 @@ from test_framework.test_utils import enable_executor_hook
 
 RED, WHITE, BLUE = range(3)
 
-
+# TC: O(N) SC: O(1)
 def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
-    return
+    pivot = A[pivot_index]
+    # Keep the following invariants during partitioning:
+    # bottom group: A[:smaller].
+    # middle group: A[smaller:equal].
+    # unclassified group: A[equal:larger].
+    # top group: A[larger:].
+    smaller, mid, larger = 0, 0, len(A)
+    # Keep iterating as long as there is an unclassified element.
+    while mid < larger:
+        # A[equal] is the incoming unclassified element.
+        if A[mid] < pivot:
+            A[smaller], A[mid] = A[mid], A[smaller]
+            smaller, mid = smaller + 1, mid + 1
+        elif A[mid] == pivot:
+            mid += 1
+        else:  # A[equal] > pivot.
+            larger -= 1
+            A[mid], A[larger] = A[larger], A[mid]
+    print(smaller, mid, larger)
+    # pivot = A[pivot_index]
+    #
+    # sm = 0
+    # for i in range(len(A)):
+    #     if A[i] < pivot:
+    #         A[i], A[sm] = A[sm], A[i]
+    #         sm += 1
+    #
+    # right = len(A) - 1
+    # for j in range(len(A) - 1, -1, -1):
+    #     if A[j] > pivot:
+    #         A[j], A[right] = A[right], A[j]
+    #         right -= 1
+
 
 
 @enable_executor_hook
@@ -39,6 +70,8 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
 
 
 if __name__ == '__main__':
+    A = [9, -1, 2, 0, 9, 2, 2, 2]
+    dutch_flag_partition(3, A)
     exit(
         generic_test.generic_test_main("dutch_national_flag.py",
                                        'dutch_national_flag.tsv',
--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,12 +2,21 @@ from test_framework import generic_test
 
 
 def is_well_formed(s):
-    # TODO - you fill in here.
-    return True
+    stack = []
+    LOOKUP = {"(": ")", "{": "}", "[": "]"}
+    for char in s:
+        if char in LOOKUP:
+            stack.append(char)
+        elif not stack or LOOKUP[stack.pop()] != char:
+            return False
+    return not stack
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("is_valid_parenthesization.py",
-                                       'is_valid_parenthesization.tsv',
-                                       is_well_formed))
+        generic_test.generic_test_main(
+            "is_valid_parenthesization.py",
+            "is_valid_parenthesization.tsv",
+            is_well_formed,
+        )
+    )
