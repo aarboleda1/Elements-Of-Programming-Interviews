a9f0ca488d16df7b5745956d39d2df6f649f50c7
Anton Arboleda <aarboleda@fb.com>
2019-03-24 17:00:07 -0700

    bst_from_pre_order

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,13 +1,33 @@
+from bst_node import BstNode
 from test_framework import generic_test
 
 
 def rebuild_bst_from_preorder(preorder_sequence):
-    # TODO - you fill in here.
-    return None
+    root_idx = [0]
+
+    def rebuild_bst_from_preorder_on_value_range(lo, hi):
+        if root_idx[0] == len(preorder_sequence):
+            return None
+
+        val = preorder_sequence[root_idx[0]]
+        # keeps the variant of a BST tree that parent is greater than left
+        # and less than right child
+        if not lo <= val <= hi:
+            return None
+
+        root_idx[0] += 1
+        root = BstNode(val)
+        print(lo, hi)
+        root.left = rebuild_bst_from_preorder_on_value_range(lo, val)
+        root.right = rebuild_bst_from_preorder_on_value_range(val, hi)
+        return root
+
+    return rebuild_bst_from_preorder_on_value_range(float("-inf"), float("inf"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("bst_from_preorder.py",
-                                       'bst_from_preorder.tsv',
-                                       rebuild_bst_from_preorder))
+        generic_test.generic_test_main(
+            "bst_from_preorder.py", "bst_from_preorder.tsv", rebuild_bst_from_preorder
+        )
+    )
