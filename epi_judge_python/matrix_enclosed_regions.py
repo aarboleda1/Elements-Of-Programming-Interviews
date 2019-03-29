51bf30178382ef59193cfb8817b6ede8bd22a828
Anton Arboleda <aarboleda@fb.com>
2019-03-28 21:31:05 -0700

    more algos

--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -1,18 +1,34 @@
+import collections
+
 from test_framework import generic_test
 
 
 def fill_surrounded_regions(board):
-    # TODO - you fill in here.
-    return
+    n, m = len(board), len(board[0])
+    q = collections.deque(
+        [(i, j) for k in range(n) for i, j in ((k, 0),(k, m - 1))] +
+        [(i, j) for k in range(m) for i, j in ((0, k), (n - 1, k))]
+    )
 
-
+    while q:
+        x, y = q.popleft()
+        if 0 <= x < n and 0 <= y < m and board[x][y] == 'W':
+            board[x][y] = 'T'
+            q.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
+    board[:] = [['B' if c != 'T' else 'W' for c in row] for row in board]
+fill_surrounded_regions([
+    ['B','B','B','B'],
+    ['W','B','W','B'],
+    ['B','W','B','B'],
+    ['B','W','B','B'],
+])
 def fill_surrounded_regions_wrapper(board):
     fill_surrounded_regions(board)
     return board
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("matrix_enclosed_regions.py",
-                                       'matrix_enclosed_regions.tsv',
-                                       fill_surrounded_regions_wrapper))
+# if __name__ == '__main__':
+    # exit(
+        # generic_test.generic_test_main("matrix_enclosed_regions.py",
+                                       # 'matrix_enclosed_regions.tsv',
+                                       # fill_surrounded_regions_wrapper))
