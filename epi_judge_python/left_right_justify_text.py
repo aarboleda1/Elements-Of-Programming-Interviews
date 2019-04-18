c76d7d9833c67bac4cd2d790efce2f0cdd955ce7
Anton Arboleda <aarboleda@fb.com>
2019-04-18 10:04:00 -0700

    some comments

--- epi_judge_python/left_right_justify_text.py
+++ epi_judge_python/left_right_justify_text.py
@@ -4,6 +4,9 @@ from test_framework import generic_test
 def justify_text(words, L):
     cur_line_len, res, cur_line = 0, [], []
     for word in words:
+        # We need the len(cur_line), because this tells us the number of spaces
+        # so for L = 14 and a set of words "is trickier than", it should
+        # be broken down to "is    tricker", "than    "
         if cur_line_len + len(word) + len(cur_line) > L:
             for i in range(L - cur_line_len):
                 cur_line[i % max(len(cur_line) - 1, 1)] += " "
