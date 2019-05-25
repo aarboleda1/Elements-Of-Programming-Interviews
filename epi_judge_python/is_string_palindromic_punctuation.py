35686446a72c0d3e5c537d30f5d5a2587de83e98
Anton Arboleda <aarboleda@fb.com>
2019-05-25 14:03:29 -0700

    is_string_palindromic_punctuation

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,13 +1,31 @@
 from test_framework import generic_test
 
 
+"""6.5 Test Palindromicity
+
+For the purpose of this problem, define a palindromic string to be a string
+which when all nonalphanumeric are removed it reads the same front to back
+ignoring case. For example, "A man, a plan, a canal, Panama" and "Able
+was I, ere I saw Elba" are palindromic, but "Ray a Ray" is not.
+
+Implement a function which takes as input a string s and returns true if s is a
+palindromic string
+
+[ATTEMPTED] 5/25
+"""
+
+
 def is_palindrome(s):
-    # TODO - you fill in here.
+
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    is_palindrome("A man, a plan, a canal, Panama.")
     exit(
-        generic_test.generic_test_main("is_string_palindromic_punctuation.py",
-                                       "is_string_palindromic_punctuation.tsv",
-                                       is_palindrome))
+        generic_test.generic_test_main(
+            "is_string_palindromic_punctuation.py",
+            "is_string_palindromic_punctuation.tsv",
+            is_palindrome,
+        )
+    )
--- epi_judge_python_solutions/is_string_palindromic_punctuation.py
+++ epi_judge_python_solutions/is_string_palindromic_punctuation.py
@@ -1,6 +1,22 @@
 from test_framework import generic_test
 
+"""6.5 Test Palindromicity
 
+For the purpose of this problem, define a palindromic string to be a string
+which when all nonalphanumeric are removed it reads the same front to back
+ignoring case. For example, "A man, a plan, a canal, Panama" and "Able
+was I, ere I saw Elba" are palindromic, but "Ray a Ray" is not.
+
+Implement a function which takes as input a string s and returns true if s is a
+palindromic string
+
+Basic Algorithm: Start and end pointers to find chars. Only subtlety is if
+s[start] or s[end] is not an alphanumberic character. Move pointers until you
+find an alphanumeric char then check their lowercase value
+
+[ATTEMPTED] 5/25
+
+"""
 def is_palindrome(s):
 
     # i moves forward, and j moves backward.
